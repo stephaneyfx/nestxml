56c8d5cd748a0114f58b3e909ae32d201cc585cd
Stephane Raux <stephaneyfx@gmail.com>
2019-02-11 23:08:02 -0800

    Normalize line endings and style

--- src/html.rs
+++ src/html.rs
@@ -6,22 +6,22 @@ use std::io::Write;
 use xml::EventWriter;
 use xml::writer::Error;
 
-element_constructor! {html}
-element_constructor! {head}
-element_constructor! {title}
-element_constructor! {meta}
-element_constructor! {style}
-element_constructor! {body}
-element_constructor! {table}
-element_constructor! {tr}
-element_constructor! {th}
-element_constructor! {td}
-element_constructor! {h1}
-element_constructor! {h2}
-element_constructor! {ul}
-element_constructor! {ol}
-element_constructor! {li}
-element_constructor! {a}
+element_constructor!(html);
+element_constructor!(head);
+element_constructor!(title);
+element_constructor!(meta);
+element_constructor!(style);
+element_constructor!(body);
+element_constructor!(table);
+element_constructor!(tr);
+element_constructor!(th);
+element_constructor!(td);
+element_constructor!(h1);
+element_constructor!(h2);
+element_constructor!(ul);
+element_constructor!(ol);
+element_constructor!(li);
+element_constructor!(a);
 
 /// Writes the html DOCTYPE declaration.
 pub fn write_doctype<W: Write>(w: &mut EventWriter<W>) -> Result<(), Error> {
